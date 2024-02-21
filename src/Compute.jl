23e43bedd7169679d01d7b230f4685ce10ab6e88
JumanaAlam <jumana.alam28@gmail.com>
2024-02-21 18:19:57 -0500

    Update Compute.jl

--- src/Compute.jl
+++ src/Compute.jl
@@ -16,6 +16,27 @@ function _decode_part_1(model::MyPuzzleRecordModel)::Int64
     return value |> join |> x -> parse(Int64,x);
 end
 
+function _decode_part_2(model::MyPuzzleRecordModel)::Int64
+    record = model.record;
+    number_library = Dict("one" => 1, "one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9, "zero" => 0)
+
+    #we are replacing numbers written in words with a numerical value. If we have a number in a word form, we add a new start and end character to it and replace the word with the new word, e.g. "eight" becomes "eeightt")
+    for (word, number) in number_library
+        if occursin(word, record)
+            #replace number word with a numerical value
+            first_char = word[1] |> string;
+            last_char = word[end] |> string;
+            replacement_word = "$(first_char)$(word)$(last_char)";
+            record = replace(record, word => replacement_word) |> record -> replace(record, word => number);
+        end
+    end
+#update the model
+model.record = record;
+model.characters = collect(record);
+
+#use _decode_part_1 to parse our value
+return _decode_part_1(model);
+end
 # ===== PUBLIC METHODS BELOW HERE ==================================================================================== #
 """
     decode_part_1(models::Dict{Int64, MyPuzzleRecordModel}) -> Tuple{Int64, Dict{Int64, Int64}}
